114c76351c0a8e02dcd17bb9ea915f51628e2062
hitesh <hitesh22rana@gmail.com>
2021-09-28 18:41:27 +0530

    Added new dark theme and more responsiveness with transitions and hover effects

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let outputScreen = document.getElementById('output-screen');
+let themebg = document.body;
+let themecal = document.getElementById("calc");
+let themebutton = document.getElementsByClassName("buttons");
+let themeequal = document.getElementsByClassName("equals");
 
 function display(num) {
     outputScreen.value += num;
@@ -20,4 +24,16 @@ function Clear() {
 
 function del() {
     outputScreen.value = outputScreen.value.slice(0 , -1);
+}
+
+function Themechange() {
+    themebg.classList.toggle("body-bg");
+    themecal.classList.toggle("calculator-theme");
+    outputScreen.classList.toggle("input-toggle");
+
+    let len = themebutton.length;
+    for(var i = 0 ; i<len ; i++) {
+        themebutton.item(i).classList.toggle("button-toggle");
+    }
+    themeequal.item(0).classList.toggle("equal-toggle");
 }
